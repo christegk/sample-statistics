42152ae72544de6297919de992f28999b86cdde9
Chris <chris_tegk@hotmail.com>
2023-05-22 17:25:07 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sample-statistics',
-    version='0.1',
+    version='1.0',
     description='A package for calculating weighted statistics on a set of samples.',
     author='Christos Tegkelidis',
     author_email='cteg@kth.se',
